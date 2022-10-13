893335abd8ee86e8166e84e7c373e652be017908
Luiz Guilherme Baptista Machado <lgbmachado@gmail.com>
2022-10-13 08:09:57 -0300

    Implementação do medidor de pico de som.

--- NinoMusicServer/Views/ViewController.swift
+++ NinoMusicServer/Views/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import Cocoa
-//import UIKit
-//import UIAlertController
+import AVFAudio
+
+enum ChanelMeter {
+    case right
+    case left
+}
 
 class ViewController: NSViewController {
+    
+    private let updateInterval = 0.05
     
     var musicsListViewModel: MusicsListViewModel?
     
     @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var btnPrevMusic: NSButton!
+    @IBOutlet weak var btnPlayPauseMusic: NSButton!
+    @IBOutlet weak var btnStopMusic: NSButton!
+    @IBOutlet weak var btnNextMusic: NSButton!
+    
+    @IBOutlet weak var leftMeter: NSLevelIndicator!
+    @IBOutlet weak var rightMeter: NSLevelIndicator!
+    
+    var player: AVAudioPlayer?
+    private var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +51,7 @@ class ViewController: NSViewController {
         
     }
     
-    @IBAction func opnDirClick(_ sender: Any) {
+    @IBAction func openDirClick(_ sender: Any) {
         let dialog = NSOpenPanel()
         dialog.title = "Selecione o diretório com as músicas"
         dialog.showsResizeIndicator = true;
@@ -52,8 +68,6 @@ class ViewController: NSViewController {
                 ListFileMusic().loadMusics(path: path) { musicsLoaded in
                     if let musicsLoaded = musicsLoaded {
                         print("👍 \(musicsLoaded) música(s) carregada(s)")
-                        //                        let alert = UIAlertController(title: "Sign out?", message: "You can always access your content by signing back in", preferredStyle: UIAlertControllerStyle.alert)
-                        //                        self.present(alert, animator: true)
                         let musicDb = Database(databasePath: "/Users/nino/MusicDatabase.db")
                         musicDb.listMusics { musicsList in
                             if let musicsList = musicsList {
@@ -68,6 +82,67 @@ class ViewController: NSViewController {
         } else {
             return
         }
+    }
+    
+    @IBAction func playPauseMusicClick(_ sender: Any) {
+        if let url = URL(string: self.musicsListViewModel?.musicAtIndex(self.tableView.selectedRow).filePath ?? "") {
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                guard let player = player else { return }
+                
+                player.prepareToPlay()
+                player.isMeteringEnabled = true
+                player.play()
+                
+                if player.isPlaying {
+                    self.leftMeter.floatValue = 0.0
+                    self.rightMeter.floatValue = 0.0
+                    
+                    timer = Timer.scheduledTimer(timeInterval: updateInterval,
+                                                 target: self,
+                                                 selector: #selector(self.updateMeters),
+                                                 userInfo: nil,
+                                                 repeats: true)
+                    
+                    btnPrevMusic.isEnabled = true
+                    btnStopMusic.isEnabled = true
+                    btnNextMusic.isEnabled = true
+                }
+                
+            } catch let error as NSError {
+                print(error.description)
+            }
+        }
+    }
+    
+    @IBAction func stopMusicClick(_ sender: Any) {
+        if let player = self.player {
+            if player.isPlaying {
+                guard timer != nil, timer!.isValid else {
+                    return
+                }
+                timer?.invalidate()
+                timer = nil
+                
+                player.stop()
+                btnPrevMusic.isEnabled = false
+                btnStopMusic.isEnabled = false
+                btnNextMusic.isEnabled = false
+                
+                self.leftMeter.floatValue = 0.0
+                self.rightMeter.floatValue = 0.0
+            }
+        }
+    }
+    
+    @objc private func updateMeters() {
+        if let player = self.player {
+            player.updateMeters()
+            self.leftMeter.floatValue = (player.peakPower(forChannel: 0) + 160.0)
+            self.rightMeter.floatValue = (player.peakPower(forChannel: 1) + 160.0)
+            print(" \(player.peakPower(forChannel: 0))")
+        }
+        
     }
     
 }
@@ -129,6 +204,15 @@ extension ViewController: NSTableViewDelegate {
             
         }
         return nil
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        if let rows = self.musicsListViewModel?.numberOfRowsInSection(1) {
+            if row >= 0 && row < rows {
+                btnPlayPauseMusic.isEnabled = true
+            }
+        }
+        return true
     }
 }
 
